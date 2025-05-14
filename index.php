4c56e918ac46e0b76a72274c3545a19505f9ff26
p <petrubraha@gmail.com>
2025-05-14 22:45:49 +0300

    installed php extension dotenv

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-header("Location: controller/controller.php");
+
+require __DIR__ . '/vendor/autoload.php';
+// header("Location: controller/controller.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dada = $_ENV['MAP_KEY'];
+echo $dada;
 exit();
