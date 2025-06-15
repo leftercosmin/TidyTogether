7d63b45e43bf0d6b76ec4d515d14234730bd8573
p <petrubraha@gmail.com>
2025-06-15 17:19:56 +0300

    moved back the vendor directory, mounted the vendor libraries

--- app/index.php
+++ app/index.php
@@ -1,15 +1,6 @@
 <?php // used to load the dependencies
 
-$path = "";
-$file = strtok(__DIR__, "/");
-while ($file !== false) {
-  if ($file !== "app")
-    $path .= $file . "/";
-  $file = strtok("/");
-}
-
-$path .= 'vendor/autoload.php';
-require_once __DIR__ . 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
 require_once 'controller/homeController.php';
