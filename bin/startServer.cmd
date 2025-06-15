88977974d4d334685d98b54ad0c039246a6f29ce
p <petrubraha@gmail.com>
2025-06-15 19:51:50 +0300

    bin directory revisited

--- bin/startServer.cmd
+++ bin/startServer.cmd
@@ -1,13 +1,8 @@
 @echo off
 
-:: should be called only once
-
 F:\xampp\xampp_start.exe
+.\bin\build.cmd
 
-set "MYSQL=F:\xampp\mysql\bin\mysql"
-set "HOST=localhost"
-set "USER=root"
-set "FILE=.\bin\schema.sql"
-
-type %FILE% | %MYSQL% -h %HOST% -u %USER% -p
-./bin/build.cmd
+if "%~1"=="reset" (
+  .\bin\resetSchema.cmd
+)
