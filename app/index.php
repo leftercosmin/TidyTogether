63ba676261d313bb6e819a39534e24d88a98a060
p <petrubraha@gmail.com>
2025-06-15 17:31:23 +0300

    env var check on wasmer

--- app/index.php
+++ app/index.php
@@ -1,6 +1,5 @@
 <?php // used to load the dependencies
 
-require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
-$dotenv->load();
+echo getenv("lala");
+echo "attention!!!!!!!!!!!!!!!!!!!!!!!!\n";
 require_once 'controller/homeController.php';
