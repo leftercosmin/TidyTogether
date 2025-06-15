fc99e1f2ee4a2ac09003a6d94724205f0cd8c795
p <petrubraha@gmail.com>
2025-06-15 16:58:56 +0300

    incercarea moarte nu are?

--- app/index.php
+++ app/index.php
@@ -1,6 +1,15 @@
 <?php // used to load the dependencies
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$path = "";
+$file = strtok(__DIR__, "/");
+while ($file !== false) {
+  if ($file !== "app")
+    $path .= $file . "/";
+  $file = strtok("/");
+}
+
+$path .= 'vendor/autoload.php';
+require_once __DIR__ . 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 require_once 'controller/homeController.php';
