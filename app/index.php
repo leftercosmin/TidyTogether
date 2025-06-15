75c2c197facd1af948bbea2a1fd4d747ff3fc3d2
p <petrubraha@gmail.com>
2025-06-15 16:36:50 +0300

    hotfix path

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php // used to load the dependencies
 
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 require_once 'controller/homeController.php';
