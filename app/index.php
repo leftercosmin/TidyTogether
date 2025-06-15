13ccf51b740b6c212582e86ef2873c76542a9c40
p <petrubraha@gmail.com>
2025-06-15 16:34:34 +0300

    moved our logic inside of the app directory

--- app/index.php
+++ app/index.php
@@ -1,3 +1,6 @@
-<?php
+<?php // used to load the dependencies
 
-echo "fasole";
+require_once __DIR__ . '../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+require_once 'controller/homeController.php';
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php // used to load the dependencies
-
-require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-require_once 'controller/homeController.php';
