6e61ed8eb100316e6188278ba45f4ab1ff8e402d
P <petrubraha@gmail.com>
2025-06-20 11:16:57 +0300

    curly brachets index

--- app/index.php
+++ app/index.php
@@ -9,8 +9,9 @@ if (!getenv('SERVER')) {
   require_once __DIR__ . '/../vendor/autoload.php';
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
   $dotenv->load();
-  foreach ($_ENV as $key => $value)
+  foreach ($_ENV as $key => $value) {
     putenv("$key=$value");
+  }
 }
 
 require_once 'controller/homeController.php';
