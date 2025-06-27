54197224808ae27e46a537ccfe847ecf3941056c
p <petrubraha@gmail.com>
2025-06-27 12:19:02 +0300

    added to navbar another page for the authority

---
+++ app/view/home/authorityAreaView.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Recycling Area</title>
+  <meta name="description" content="Authority dashboard for managing approved reports">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style/globals.css">
+  <link rel="stylesheet" href="style/navbar.css">
+  <link rel="stylesheet" href="style/authorityArea.css">
+</head>
+
+<body>
+
+  <?php require_once 'view/components/authorityNavbar.php'; ?>
+
+  <h1 class="page-title">Recycling Area</h1>
+
+  <script src="javascript/navbarCollapse.js"></script>
+</body>
+
+</html>
